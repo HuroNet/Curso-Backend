acf866a3910dcc1dff2496ba650a508c000e55f9
Huron <charlie.walls97@gmail.com>
2024-06-26 19:01:44 -0500

    pk

--- HTML/server.js
+++ HTML/server.js
@@ -6,7 +6,10 @@ app.set('view engine', 'ejs'); // view engine (motor de plantillas)
 
 
 //middleware
-app.use('/assets',express.static('assets')); 
+app.use('/assets',express.static('assets',{
+        atag:false,
+        maxAge:'5h'
+})); 
 
 
 app.get('/', function(req,res){
