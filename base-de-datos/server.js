208f04bc7adcfd516c81ebd307e9ef6fba861cd2
Huron <charlie.walls97@gmail.com>
2024-06-27 15:45:28 -0500

    add more things of backend course

--- base-de-datos/server.js
+++ base-de-datos/server.js
@@ -1,10 +1,26 @@
 
 const sqlite3 = require('sqlite3')
 const express = require('express');
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+
 const app = express();
-let db = new sqlite3.Database('proyecto-backend');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+// let db = new sqlite3.Database('proyecto-backend');
 
 
-db.run('CREATE TABLE task(id int AUTO_INCREMENT,description varchar(244))');
-db.close();
+const sequelize = new Sequelize('proyecto-backend', null, null,{
+    dialect:'sqlite',
+    Storage:'./Curso-Backend'
+});
+
+app.post('/pendientes',function(req,res){
+    res.send('Bien hecho, insersion completada');
+});
+
+
+// db.run('CREATE TABLE task(id int AUTO_INCREMENT,description varchar(244))');
+// db.close();
 app.listen(3002);
